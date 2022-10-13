17daf197eec654800bbd1b8d89135814db267949
aniketjain8441 <aniketjain8441@gmail.com>
2022-10-13 12:11:48 +0530

    master
    "hacktoberfest"

---
+++ scr/factory/CommercialPlan.java
@@ -0,0 +1,8 @@
+class CommercialPlan extends Plan {
+
+  //@override
+  public void getRate() {
+    rate = 7.50;
+  }
+}
+//end of CommercialPlan class.
---
+++ scr/factory/InstitutionalPlan.java
@@ -0,0 +1,8 @@
+class InstitutionalPlan extends Plan {
+
+  //@override
+  public void getRate() {
+    rate = 5.50;
+  }
+}
+//end of InstitutionalPlan class.
